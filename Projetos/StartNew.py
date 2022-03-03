bcde6aa27885245bd6487b26b45d1aa59a0e3f5c
RadioD <43344590+D0nnieDark0@users.noreply.github.com>
2022-03-03 12:20:20 -0300

    Update StartNew.py

--- Projetos/StartNew.py
+++ Projetos/StartNew.py
@@ -5,11 +5,15 @@ print(datetime.now())
 
 # * Primeira parte do codigo Acessando o SAP e baixando os arquivos
 
+USUARIO = ""
+SENHA = ""
+NUMERO_TRANSACAO = ""
+
 s = Sap()
 
-s.login("GUISOUSA", "A2U2!NKfhH8n#$w")
+s.login(USUARIO, SENHA)
 
-s.transaction("me5a")
+s.transaction(NUMERO_TRANSACAO)
 
 s.purchase()
 
